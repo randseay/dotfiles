a97394c0ad762c6cf83d94341082aa6b0b09751a
Rand Seay <rand.seay@gmail.com>
2014-05-26 00:40:29 -0500

    Update color in day_time script.

--- zsh/zsh_day_time.py
+++ zsh/zsh_day_time.py
@@ -5,8 +5,8 @@ import sys, time, datetime
 
 day = str(time.strftime("%a %H:%M"))
 
-color_teal = '%{$FG[101]%}'
+color_forest = '%{$FG[101]%}'
 color_reset = '%{$FG[240]%}'
 
-out = color_teal + day + color_reset
+out = color_forest + day + color_reset
 sys.stdout.write(out)
